df35d76d5e6d82342f8e7620e4e653fe503f216e
wajih <wajih.mejri@esprit.tn>
2023-01-01 11:33:05 +0100

    nom ep

--- Views/sidebarEpi.php
+++ Views/sidebarEpi.php
@@ -3,7 +3,7 @@
 <div class="sidenav-header">
   <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
   <a class="navbar-brand m-0" href="" target="_blank">
-    <span class="ms-1 font-weight-bold text-white"> <?php echo $nom ?></span>
+    <span class="ms-1 font-weight-bold text-white"> Episode : <?php echo $nom ?></span>
   </a>
 </div>
 
